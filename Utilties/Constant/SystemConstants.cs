58c7302ddac3da856d8955619c5013355d1c24d8
Đình Dương Nguyễn <nguyendinhduongrbt@gmail.com>
2021-05-22 15:08:30 +0700

    add swagger

--- Utilties/Constant/SystemConstants.cs
+++ Utilties/Constant/SystemConstants.cs
@@ -6,6 +6,6 @@ namespace Utilties.Constant
 {
     public class SystemConstants
     {
-        public const string MainConnectionString = "LongVietWeb_Database";
+        public const string MainConnectionString = "Web_App";
     }
 }
